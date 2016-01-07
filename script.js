4279ee0a5df28c4624ea8b74c72f3bd5450c8924
Tad <tad@whitakerresearch.com>
2016-01-07 10:12:39 -0800

    Palindrome Checker

--- script.js
+++ script.js
@@ -14,7 +14,21 @@
 var a = "abba";
 var b = "sidney";
 
-var checkPalindrome = function(str) {
-    return str == str.split('').reverse().join('');
+var palindromeChecker = function(n) {
+  var regular = n.split("");
+  var reversed = n.split("").reverse();
+  var answer = true;
+
+  for (var i = 0; i < regular.length; ) {
+    if (regular[i] === reversed[i]) {
+      i++;
+    } else {
+      answer = false;
+      i++;
+    }
+  }
+
+  return answer;
 };
 
+palindromeChecker(b);
