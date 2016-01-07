8f204d276c8265bee50835f55065fffb359300d1
Tad <tad@whitakerresearch.com>
2016-01-07 10:37:20 -0800

    Add digits of an integer until result has only one digit.

--- script.js
+++ script.js
@@ -11,24 +11,60 @@
 // };
 
 // Palindrome
-var a = "abba";
-var b = "sidney";
+// var a = "abba";
+// var b = "sidney";
 
-var palindromeChecker = function(n) {
-  var regular = n.split("");
-  var reversed = n.split("").reverse();
-  var answer = true;
+// var palindromeChecker = function(n) {
+//   var regular = n.split("");
+//   var reversed = n.split("").reverse();
+//   var answer = true;
 
-  for (var i = 0; i < regular.length; ) {
-    if (regular[i] === reversed[i]) {
-      i++;
-    } else {
-      answer = false;
-      i++;
-    }
-  }
+//   for (var i = 0; i < regular.length; ) {
+//     if (regular[i] === reversed[i]) {
+//       i++;
+//     } else {
+//       answer = false;
+//       i++;
+//     }
+//   }
 
-  return answer;
-};
+//   return answer;
+// };
 
-palindromeChecker(b);
+// palindromeChecker(b);
+
+// Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+// var number = function(n) {
+//   var digits = ("" + n).split("");
+//   var answer = parseInt(digits[0]) + parseInt(digits[1]);
+//   var answeredString = answer.toString();
+//   if (answeredString.length === 1) {
+//     return;
+//   } else {
+//     number(answer);
+//   }
+// };
+
+// number(38);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
